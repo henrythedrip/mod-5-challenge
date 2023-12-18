a67b0345963adc78b46e6998ff12cc88b20b2b8b
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2023-12-18 14:13:03 -0700

    finished functionality

--- Develop/script.js
+++ Develop/script.js
@@ -3,32 +3,99 @@ console.log('Smoke Test: ', typeof $);
 
 
 
+
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// ✅ THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// ✅ THEN I am presented with time blocks for standard business hours of 9am to 5pm
+// WHEN I view the time blocks for that day
+// ✅ THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
+// ✅ THEN I can enter an event
+// WHEN I click the save button for that time block
+// ✅ THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// ✅ THEN the saved events persist
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html. ✅
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should ✅
-  // use the id in the containing time-block as a key to save the user input in 
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+    
+  // https://day.js.org/docs/en/installation/installation
+  // handle adding todays date at top of page
+  // add one to month because its zero-indexed
+  const month = dayjs().get('month') + 1; 
+  const day = dayjs().get('date')
+  const year = dayjs().get('year')
+  const hour = dayjs().get('hour')
+  const dateString = (month+'/'+day+'/'+year);
 
-  /* hour block 
-  <div id="hour-11" class="row time-block future">
-    <div class="col-2 col-md-1 hour text-center py-3">11AM</div>
+  // append the date
+  $('#currentDay').append(dateString);
+  $('#clearButton').click(() => {
+    localStorage.clear();
+    location.reload();
+  });
+
+  
+  // handle hour parts of today
+  // const hoursInDay = [9, 10, 11, 12, 1, 2, 3, 4];
+  const hoursInDay = [9, 10, 11, 12, 13, 14, 15, 16]; 
+  // dayjs gives us 24h time
+  hoursInDay.forEach(workHour => {
+    
+    // create class name for hour based on current time
+    const pastOrPresent = workHour < hour ? 'past' : 
+    workHour == hour ? 'present' : 'future' 
+    console.log(workHour, hour, pastOrPresent)
+    
+    // https://stackoverflow.com/questions/10619445/the-preferred-way-of-creating-a-new-element-with-jquery
+    // creating elements with jquery
+
+    /* hour block 
+    <div id="hour-11" class="row time-block future">
+      <div class="col-2 col-md-1 hour text-center py-3">11AM</div>
+      <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+        <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+    </div>
+    */
+    
+    // create an hour block for each working hour
+    var $div = $("<div>", {
+      id: 'hour-' + workHour,
+      class: `row time-block ${pastOrPresent}`
+    });
+
+    const AMPM = workHour < 12 ? 'AM' : 'PM'
+    const newHour = workHour > 12 ? workHour - 12 : workHour
+
+    // append in the rest of the stuff
+    // https://api.jquery.com/html/
+    $div.html(`<div class="col-2 col-md-1 hour text-center py-3">${newHour + ':00' + AMPM}</div>
     <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
     <button class="btn saveBtn col-2 col-md-1" aria-label="save">
       <i class="fas fa-save" aria-hidden="true"></i>
-    </button>
-  </div>
-  */
+    </button>`)
+    
 
+    $(".container-fluid").append($div);
+    
+
+  });
+  
+  
   // might want to do: we probably want to add in more time blocks so that we have enough for each hour of the day. we can do this by creating elements, setting their attributes, appending them to a container element, and then appending that container element to the webpage.
   
   // attempt to find all save buttons and add an event listener to them as a smoke test
   $('.time-block').each((index, timeBlock) => {
     // find the id of the timeBlock - we will use this as our localstorage key when we save stuff
     const containerID = $(timeBlock).attr('id');
-
+    
     // find the save button inside of the time block 
     // https://api.jquery.com/children/
     const saveButton = $(timeBlock).children('.saveBtn');
@@ -43,12 +110,7 @@ $(function () {
     })
 
     // get stuff from localstorage and show it in the textarea
-    $(textArea).val(localStorage.getItem(containerID));
-  
-    // doumentation for day.js
-    // https://day.js.org/docs/en/installation/installation
-
-    
+    $(textArea).val(localStorage.getItem(containerID));    
   })
 });
 
